d598a83a884d71e8ec5852e4daf9f3b89aa04080
Erik Marsh <erik.i.marsh@gmail.com>
2022-12-13 16:27:36 -0800

    Trivial input file parsing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
+#include <fstream>
 #include <iostream>
+#include <regex>
 #include <string>
+#include <vector>
 
 const std::string CONFIG_FILENAME = "input.conf";
 
@@ -36,15 +39,84 @@ struct InputConfig
 };
 
 const InputConfig GetInputConfig(const std::string& inputFilename);
+const std::vector<std::string> WhitespaceTokenizer(const std::string& line);
 
 int main()
 {
+    GetInputConfig(CONFIG_FILENAME);
     return 0;
 }
 
+// simple, non-generalizable parser
 const InputConfig GetInputConfig(const std::string& inputFilename)
 {
     InputConfig config;
+    std::ifstream file(inputFilename);
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+        // if (line[0] == COMMENT_INDICATOR)
+        //     continue;
+        
+        auto tokens = WhitespaceTokenizer(line);
+        // not-totally-expected behavior: strings starting in whitespace will return an empty string for the first token
+
+        // this should never be the case, but I'll write it just in case
+        if (tokens.size() == 0) continue;
+
+        // empty line
+        if (tokens.size() == 1 && tokens[0] == "")
+            continue;
+
+        // now we have the option of ignoring the initial whitespace
+        if (tokens.size() > 1 && tokens[0] == "")
+            tokens.erase(tokens.begin());
+
+        // ignore comments
+        if (tokens[0][0] == COMMENT_INDICATOR)
+            continue;
+
+        std::string key = "";
+        std::string value0 = "";
+        std::string value1 = "";
+
+        // parse content lines
+        // anything that isn't valid should be ignored
+        if (*(tokens[0].end() - 1) == NAME_VALUE_DELIMITER)
+        {
+            if (tokens.size() < 2) continue;
+
+            key = {tokens[0].begin(), tokens[0].end() - 1}; // truncate the :
+
+            std::cout << "[key detected]: " << key << std::endl;
+            if (*(tokens[1].end() - 1) == TUPLE_DELIMITER)
+            {
+                if (tokens.size() < 3) continue;
+
+                value0 = {tokens[1].begin(), tokens[1].end() - 1};
+                value1 = tokens[2];
+
+                std::cout << "[first value must be]: " << value0 << std::endl;
+                std::cout << "[found second value]: " << value1 << std::endl;
+            }
+            else
+            {
+                value0 = tokens[1];
+                std::cout << "[first value must be]: " << value0 << std::endl;
+            }
+        }
+    }
 
     return config;
+}
+
+// shout out to StackOverflow
+// https://stackoverflow.com/questions/9435385/split-a-string-using-c11
+const std::vector<std::string> WhitespaceTokenizer(const std::string& line)
+{
+    std::regex re("\\s+");
+    std::sregex_token_iterator first {line.begin(), line.end(), re, -1};
+    std::sregex_token_iterator last;
+    return {first, last}; // this is really neat
 }
